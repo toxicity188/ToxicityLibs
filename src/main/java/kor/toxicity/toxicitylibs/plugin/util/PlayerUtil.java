71b9d877d60038e01de3eb1d7e3c27c585a66417
toxicity188 <114675706+toxicity188@users.noreply.github.com>
2023-08-31 05:15:12 +0900

    유틸 최신화

--- src/main/java/kor/toxicity/toxicitylibs/plugin/util/PlayerUtil.java
+++ src/main/java/kor/toxicity/toxicitylibs/plugin/util/PlayerUtil.java
@@ -14,7 +14,7 @@ public class PlayerUtil {
         throw new RuntimeException();
     }
     public static int emptySpace(Player player) {
-        Inventory inv = player.getInventory();
+        var inv = player.getInventory();
         int r = 0;
         for (int i = 0; i < 36; i++) {
             var item = inv.getItem(i);
@@ -23,13 +23,19 @@ public class PlayerUtil {
         return r;
     }
     public static int storage(@NotNull Player player, @Nullable ItemStack target) {
-        if (target == null || target.getType() == Material.AIR) return emptySpace(player);
-        Inventory inv = player.getInventory();
+        if (target == null || switch (target.getType()) {
+            case AIR,CAVE_AIR,VOID_AIR -> true;
+            default -> false;
+        }) return emptySpace(player);
+        var inv = player.getInventory();
         int max = target.getMaxStackSize();
         return IntStream.range(0,36).map(i -> {
             ItemStack item = inv.getItem(i);
             if (item != null) {
-                if (item.getType() == Material.AIR) return max;
+                if (switch (item.getType()) {
+                    case AIR,CAVE_AIR,VOID_AIR -> true;
+                    default -> false;
+                }) return max;
                 else if (item.isSimilar(target)) return Math.max(max - item.getAmount(), 0);
                 else return 0;
             } else return max;
